5e7c33f3289583acd360605349e790eb045add15
Abdug'ani Toshmukhamedov <a.toshmukhamedov@icloud.com>
2024-03-03 20:57:21 +0500

    change `bat` theme

--- files/fish/conf.d/alias.fish
+++ files/fish/conf.d/alias.fish
@@ -1,10 +1,11 @@
 # Modern 'ls' command
-alias ls='eza -Gs type --icons' # --grid --sort
-alias ll='eza -als type --icons' # --all --long --sort
+alias ls='eza --grid --icons --sort type'
+alias ll='eza --all --long --icons --sort type'
+alias la='eza --all --long --icons --header --sort type'
 
 # Modern 'cat' command
-alias show='bat --theme="base16"'
-alias cat='bat --theme="base16"'
+alias show='bat --theme="OneHalfDark"'
+alias cat='bat --theme="OneHalfDark"'
 
 # Find 
 alias find='fd'
