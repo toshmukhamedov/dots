ffa9ddf27f7da46950d4b7b0a828fb04cfb530bd
Abdugani Toshmukhamedov <a.toshmukhamedov@icloud.com>
2025-05-04 23:41:20 +0500

    Add initial Home Manager configuration

--- flake.nix
+++ flake.nix
@@ -5,48 +5,78 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Home manager
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim pkgs.nixd pkgs.nil
-        ];
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      home-manager,
+      nixpkgs,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.vim
+            pkgs.nixd
+            pkgs.nil
+            pkgs.nixfmt-rfc-style
+          ];
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      programs.fish.enable = true;
+          # Enable alternative shell support in nix-darwin.
+          programs.fish.enable = true;
 
-      # Set Fish as the default shell
-      users.knownUsers = ["abdugani"];
-      users.users = {
-        abdugani = {
-          shell = pkgs.fish;
-          uid = 501;
+          # Set Fish as the default shell
+          users.knownUsers = [ "abdugani" ];
+          users.users = {
+            abdugani = {
+              shell = pkgs.fish;
+              uid = 501;
+              home = "/Users/abdugani";
+            };
+          };
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
         };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Abduganis-MacBook-Pro
+      darwinConfigurations."Abduganis-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.abdugani = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+
+          }
+        ];
       };
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Abduganis-MacBook-Pro
-    darwinConfigurations."Abduganis-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
 }
