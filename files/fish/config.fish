e7b2f332cec2023a5b85ec4cd1154e1c44cf2063
Abdugani Toshmukhamedov <a.toshmukhamedov@icloud.com>
2025-12-25 22:12:45 +0500

    update

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -24,3 +24,11 @@ direnv hook fish | source
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# Added by Antigravity
+fish_add_path /Users/abdugani/.antigravity/antigravity/bin
+
